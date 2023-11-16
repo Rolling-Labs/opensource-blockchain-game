517ba7dc7d10e871188a4cec689e57f49b2659dd
BinZ <rollinglabs@proton.me>
2023-11-17 03:32:23 +0800

    WIP

--- apps/web/views/ApexArenaView.tsx
+++ apps/web/views/ApexArenaView.tsx
@@ -1,13 +1,29 @@
 import React from "react";
 import StoneBig from "@/public/assets/apex-arena/starting-window/stone-big.svg";
+import { cn } from "@/lib/utils";
+import { spacegrotesk, trispace } from "@/public/fonts";
+import ApexArena from "@/public/assets/title/apex-arena-title.svg";
+import Image from "next/image";
+
 const ApexArenaView = () => {
   return (
-    <div className="flex justify-center items-center bg-[url('/assets/apex-arena/starting-window/apex-bg.png')] bg-no-repeat bg-cover bg-center h-[58.8rem] relative -z-[2]">
-      {/* <div className=" h-full w-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 -z-[1] bg-white" /> */}
-      <div className="flex flex-col max-w-[75rem] grow items-center bg-white">
-        <StoneBig />
+    <div className="flex justify-center items-center bg-[url('/assets/apex-arena/starting-window/apex-bg.png')] bg-no-repeat bg-cover bg-center h-[58.8rem] relative -z-[3]">
+      <div className=" h-full w-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 -z-[2] bg-cNeutral-950/50 backdrop-blur" />
+      <div className="flex flex-col max-w-[75rem] grow items-center justify-center relative ">
+        <StoneBig className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 -z-[1] w-full max-w-[70rem]" />
+        <div className="flex flex-col max-w-[50rem] grow w-full">
+          <div className="flex flex-col gap-2 items-center justify-center">
+            <img
+              height={400}
+              width={400}
+              className="w-full"
+              src={"/assets/title/apex-arena-title-v2.png"}
+              alt={"apex-arena"}
+            />
+            <ApexArena />
+          </div>
+        </div>
       </div>
-      <h1 className="text-white">asdasd</h1>
     </div>
   );
 };
